546c948f734e8be3969745ce8e47aef9818c8f09
snaranjo <unheroicmail@gmail.com>
2022-01-19 16:09:30 -0500

    adding __repr__ function to Batch

--- src/model.py
+++ src/model.py
@@ -1,7 +1,9 @@
 from datetime import date
-from typing import Optional
+from typing import Optional, Set
 
 from pydantic import BaseModel
+
+import uuid
 
 
 class OrderLine(BaseModel):
@@ -17,13 +19,30 @@ class Batch:
     def __init__(self, reference: str, sku: str, qty: int, eta: Optional[date]) -> None:
         self.reference = reference
         self.sku = sku
-        self.avaliable_quantity = qty
         self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()  # NO REPEATED orderlines can be used.
 
     def allocate(self, line: OrderLine) -> None:
-        self.avaliable_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def avaliable_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     # an orderline can only be allocated if there is a corresponding batch with a matching SKU
     # and the avaliable_quantity being less than or equal to the quantity ordered.
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.avaliable_quantity >= line.qty
+
+    def __repr__(self):
+        return f"{type(self).__name__}(reference={self.reference}, sku={self.sku}, eta={self.eta}, purchased_quantity={self._purchased_quantity}, allocated_quantity={self.allocated_quantity}, avaliable_quantity={self.avaliable_quantity})"

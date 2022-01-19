1adcda527d7bdba7097d07a71ac7eabb58d7e195
snaranjo <unheroicmail@gmail.com>
2022-01-19 14:03:04 -0500

    adding can_allocate function

--- src/model.py
+++ src/model.py
@@ -22,3 +22,8 @@ class Batch:
 
     def allocate(self, line: OrderLine) -> None:
         self.avaliable_quantity -= line.qty
+
+    # an orderline can only be allocated if there is a corresponding batch with a matching SKU
+    # and the avaliable_quantity being less than or equal to the quantity ordered.
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.avaliable_quantity >= line.qty
